9efba582ff330e711b9d72331c9ae267a5fd6667
dreedcon <ssaezcalero@gmail.com>
2015-12-14 14:52:27 +0100

    terminando graph
    pfff debemos re-mirarlo

--- Prog2_videogames/Prog2_videogames/P2Graph.h
+++ Prog2_videogames/Prog2_videogames/P2Graph.h
@@ -2,12 +2,18 @@
 #define _GRAPH_H_
 #include "Utilities.h"
 #include "P2Linked_List.h"
+#include "P2DynArray.h"
 
 template<class VAR>
-struct node
+struct Node
 {
-	VAR data;
-	list<node>* links;
+	VAR value = NULL;
+	P2DynArray<node>* links;
+
+	node(const VAR& data)
+	{
+		value = data;
+	}
 	
 };
 
@@ -15,13 +21,53 @@ template<class VAR>
 class Graph
 {
 
-	list<node>* nodes;
+	list<Node>* nodes = NULL;
 
 public:
 	 
 	Graph();
    ~Graph();
-	void AddLink(VAR* new_data, uint pos){}
+	
+   void AddNode(const VAR& new_data,const Node* father = NULL)
+   {
+	   NodeGraph<VAR*> new_node = new Node<VAR*>(new_data);
+
+	   if (father != NULL)
+	   {
+		   father->links->Pushback(new_node);
+	   }
+
+	   nodes->PushBack(new_node);
+
+	   if (start == NULL)
+		   start = new_node;
+
+	   p2List<Node*> tmp = start;
+   }
+
+   void LinkNodes(Node* father, Node* child)
+   {
+	   father->links->Pushback(fill);
+   }
+  
+   Node* GetNode(int position)
+   {
+	   p2List<Node*> tmp = start;
+	   int i = 0;
+	   while (tmp->next)
+	   {
+		   if (position = i)
+		   {
+			   break;
+		   }
+
+		   i++;
+	   }
+	   return tmp;
+   }
+
+
+
 
 
 };
